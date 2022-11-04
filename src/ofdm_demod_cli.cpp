96cd2f9fc8baf1aab69e2a62c0eb984950ce9b7d
William Yang <williamyang2017@gmail.com>
2022-11-05 02:42:35 +1100

    Added app to convert between hard and soft decision viterbi

--- src/ofdm_demod_cli.cpp
+++ src/ofdm_demod_cli.cpp
@@ -45,7 +45,7 @@ int main(int argc, char** argv)
     char* wr_filename = NULL;
 
     int opt; 
-    while ((opt = getopt(argc, argv, "b:i:M:Dh")) != -1) {
+    while ((opt = getopt(argc, argv, "b:i:o:M:Dh")) != -1) {
         switch (opt) {
         case 'b':
             block_size = (int)(atof(optarg));
