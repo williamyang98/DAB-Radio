6d8f394b3bc68fc59a65ccf3a6b65d02b6d86a8e
William Yang <williamyang2017@gmail.com>
2022-11-16 01:22:57 +1100

    Updated frequency shift tool

--- src/apply_frequency_shift.cpp
+++ src/apply_frequency_shift.cpp
@@ -9,60 +9,64 @@
 #include <fcntl.h>
 #endif
 
+#include "modules/ofdm/quantized_oscillator.h"
 #include "utility/getopt/getopt.h"
 #include "utility/span.h"
 
-float ApplyFrequencyShift(
-    tcb::span<const std::complex<uint8_t>> x, tcb::span<std::complex<uint8_t>> y, 
-    const float frequency, float dt=0.0f, const float Ts=1.0f/2.048e6)
-{
-    const size_t N = x.size();
-    for (int i = 0; i < N; i++) {
-        auto pll = std::complex<float>(
-            std::cos(dt),
-            std::sin(dt));
-        auto v_in = std::complex<float>(
-            (float)(x[i].real()) - 128.0f,
-            (float)(x[i].imag()) - 128.0f);
-        auto v_out = v_in * pll;
-        y[i] = std::complex<uint8_t>(
-            (uint8_t)(v_out.real() + 128.0f),
-            (uint8_t)(v_out.imag() + 128.0f));
-        dt += 2.0f * (float)M_PI * frequency * Ts;
-        // prevent precision errors when we have a large frequency
-        dt = std::fmod(dt, 2.0f*(float)M_PI);
-    }
-    return dt;
-}
+constexpr float DC_LEVEL = 128.0f;
+constexpr float Fs = 2.048e6f;
+constexpr float Ts = 1.0f/Fs;
+
+float ApplyPLL(
+    tcb::span<const std::complex<float>> x, tcb::span<std::complex<float>> y, 
+    const float freq_offset, const float dt0);
+
+float ApplyPLLQuantized(
+    QuantizedOscillator& oscillator,
+    tcb::span<const std::complex<float>> x, tcb::span<std::complex<float>> y, 
+    const float freq_offset, const float dt0);
+
+void ByteToFloat(tcb::span<const std::complex<uint8_t>> x, tcb::span<std::complex<float>> y);
+void FloatToByte(tcb::span<const std::complex<float>> x, tcb::span<std::complex<uint8_t>> y);
 
 void usage() {
     fprintf(stderr, 
-        "simulate_transmitter, produces OFDM data as raw IQ values\n\n"
+        "apply_frequency_shift, applies frequency shift to raw IQ values\n\n"
         "\t[-f frequency shift in Hz (default: 0)\n"
         "\t[-b block size (default: 8192)\n"
+        "\t[-i input filename (default: None)]\n"
+        "\t    If no file is provided then stdin is used\n"
+        "\t[-o output filename (default: None)]\n"
+        "\t    If no file is provided then stdout is used\n"
+        "\t[-Q Disable quantised oscillator\n"
         "\t[-h (show usage)]\n"
     );
 }
 
-int main(int argc, char** argv) 
-{
-
-#ifdef _WIN32
-    _setmode(_fileno(stdin), _O_BINARY);
-    _setmode(_fileno(stdout), _O_BINARY);
-#endif
-
+int main(int argc, char** argv) {
     float frequency_shift = 0;
     int block_size = 8192;
+    char* rd_filename = NULL;
+    char* wr_filename = NULL;
+    bool is_quantised = true;
 
     int opt; 
-    while ((opt = getopt(argc, argv, "f:b:h")) != -1) {
+    while ((opt = getopt(argc, argv, "f:b:i:o:Qh")) != -1) {
         switch (opt) {
         case 'f':
             frequency_shift = (float)(atof(optarg));
             break;
         case 'b':
             block_size = (int)(atof(optarg));
+            break;
+        case 'i':
+            rd_filename = optarg;
+            break;
+        case 'o':
+            wr_filename = optarg;
+            break;
+        case 'Q':
+            is_quantised = false;
             break;
         case 'h':
         default:
@@ -82,20 +86,52 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    auto rx_in = std::vector<std::complex<uint8_t>>(block_size);
-    float dt = 0.0f;
+    FILE* fp_in = stdin;
+    if (rd_filename != NULL) {
+        fp_in = fopen(rd_filename, "rb");
+        if (fp_in == NULL) {
+            fprintf(stderr, "Failed to open file for reading\n");
+            return 1;
+        }
+    }
 
+    FILE* fp_out = stdout;
+    if (wr_filename != NULL) {
+        fp_out = fopen(wr_filename, "wb+");
+        if (fp_out == NULL) {
+            fprintf(stderr, "Failed to open file for writing\n");
+            return 1;
+        }
+    }
+
+#ifdef _WIN32
+    _setmode(_fileno(fp_in), _O_BINARY);
+    _setmode(_fileno(fp_out), _O_BINARY);
+#endif
+
+    auto rx_in = std::vector<std::complex<uint8_t>>(block_size);
+    auto rx_float = std::vector<std::complex<float>>(block_size);
+    float dt = 0.0f;
     while (true) {
         const size_t N = rx_in.size();
-        const size_t nb_read = fread(rx_in.data(), sizeof(std::complex<uint8_t>), N, stdin);
+        const size_t nb_read = fread(rx_in.data(), sizeof(std::complex<uint8_t>), N, fp_in);
         if (nb_read != N) {
             fprintf(stderr, "Failed to read in block %zu/%zu\n", nb_read, N);
             break;
         }
 
-        dt = ApplyFrequencyShift(rx_in, rx_in, frequency_shift, dt);
+        ByteToFloat(rx_in, rx_float);
 
-        const size_t nb_write = fwrite(rx_in.data(), sizeof(std::complex<uint8_t>), N, stdout);
+        if (is_quantised) {
+            static auto oscillator = QuantizedOscillator(5, (int)(Fs));
+            dt = ApplyPLLQuantized(oscillator, rx_float, rx_float, frequency_shift, dt);
+        } else {
+            dt = ApplyPLL(rx_float, rx_float, frequency_shift, dt);
+        }
+
+        FloatToByte(rx_float, rx_in);
+
+        const size_t nb_write = fwrite(rx_in.data(), sizeof(std::complex<uint8_t>), N, fp_out);
         if (nb_write != N) {
             fprintf(stderr, "Failed to write out frame %zu/%zu\n", nb_write, N);
             break;
@@ -103,4 +139,61 @@ int main(int argc, char** argv)
     }
 
     return 0;
+}
+
+float ApplyPLL(
+    tcb::span<const std::complex<float>> x, tcb::span<std::complex<float>> y, 
+    const float freq_offset, const float dt0) 
+{
+    const int N = (int)x.size();
+
+    float dt = dt0;
+    for (int i = 0; i < N; i++) {
+        const auto pll = std::complex<float>(
+            std::cos(dt),
+            std::sin(dt));
+        y[i] = x[i] * pll;
+        dt += 2.0f * (float)M_PI * freq_offset * Ts;
+
+        // stop precision loss when going to large values
+        dt = std::fmod(dt, 2.0f*(float)M_PI);
+    }
+    return dt;
+}
+
+float ApplyPLLQuantized(
+    QuantizedOscillator& oscillator,
+    tcb::span<const std::complex<float>> x, tcb::span<std::complex<float>> y, 
+    const float freq_offset, const float dt0) 
+{
+    const int N = (int)x.size();
+    const int K = (int)oscillator.GetFrequencyResolution();
+    const int M = (int)oscillator.GetTableSize();
+    const int step = (int)freq_offset / (int)K;
+
+    int dt = (int)(dt0);
+    dt = ((dt % M) + M) % M;
+    for (int i = 0; i < N; i++) {
+        y[i] = x[i] * oscillator[dt];
+        dt = (dt + step + M) % M;
+    }
+    return (float)(dt);
+}
+
+void ByteToFloat(tcb::span<const std::complex<uint8_t>> x, tcb::span<std::complex<float>> y) {
+    const size_t N = x.size();
+    for (int i = 0; i < N; i++) {
+        y[i] = std::complex<float>(
+            (float)(x[i].real()) - DC_LEVEL,
+            (float)(x[i].imag()) - DC_LEVEL);
+    }
+}
+
+void FloatToByte(tcb::span<const std::complex<float>> x, tcb::span<std::complex<uint8_t>> y) {
+    const size_t N = x.size();
+    for (int i = 0; i < N; i++) {
+        y[i] = std::complex<uint8_t>(
+            (uint8_t)(x[i].real() + DC_LEVEL),
+            (uint8_t)(x[i].imag() + DC_LEVEL));
+    }
 }
