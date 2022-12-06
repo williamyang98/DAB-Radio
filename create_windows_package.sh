391203ba97a04e667efc2710bb0421a3950bd496
William Yang <williamyang2017@gmail.com>
2022-12-06 13:43:08 +1100

    Update windows packaging script

--- create_windows_package.sh
+++ create_windows_package.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 output_dir="dab_radio_windows_x64"
 build_dir="build/Release"
+rm -rf ${output_dir}
 mkdir -p ${output_dir}
 cp ${build_dir}/*.exe ${output_dir}/
 cp ${build_dir}/*.dll ${output_dir}/
 cp -rf bin/ ${output_dir}/
 cp -rf res/ ${output_dir}/
 cp *.ini ${output_dir}/
+cp get_live_data.sh ${output_dir}/get_live_data.sh
 cp README.md ${output_dir}/
