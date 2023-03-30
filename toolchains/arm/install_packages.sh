0283a3ebc156a8cd51c37d33fd5cfdfe9cae074a
William Yang <williamyang2017@gmail.com>
2023-03-31 05:29:28 +1100

    Updated github actions

--- toolchains/arm/install_packages.sh
+++ toolchains/arm/install_packages.sh
@@ -4,5 +4,4 @@ sudo apt-get --yes install qemu-user qemu-user-static gcc-aarch64-linux-gnu g++-
 sudo dpkg --add-architecture arm64
 sudo cat ./toolchains/arm/sources.list | sudo tee -a /etc/apt/sources.list
 sudo apt-get --yes update
-sudo apt-get --yes --fix-broken install
 sudo apt-get --yes install libglfw3-dev:arm64 libopengl-dev:arm64 libportaudio2:arm64 portaudio19-dev:arm64 librtlsdr-dev:arm64 libfftw3-dev:arm64 libfftw3-single3:arm64
--- toolchains/x86/linux-gnu-toolchain.cmake
+++ toolchains/x86/linux-gnu-toolchain.cmake
@@ -1,4 +1,2 @@
-set(CMAKE_SYSTEM_NAME Linux)
-
 set(CMAKE_C_COMPILER   "/usr/bin/gcc")
 set(CMAKE_CXX_COMPILER "/usr/bin/g++")
