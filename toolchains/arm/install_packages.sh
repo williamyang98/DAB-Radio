6dfa32ac8c3453f4e6e3e8c0e57ad22a89d08c6c
William Yang <williamyang2017@gmail.com>
2023-07-10 23:31:45 +1000

    Manually install dependencies for arm due to apt misbehaving

--- toolchains/arm/install_packages.sh
+++ toolchains/arm/install_packages.sh
@@ -4,4 +4,9 @@ sudo apt-get --yes install qemu-user qemu-user-static gcc-aarch64-linux-gnu g++-
 sudo dpkg --add-architecture arm64
 sudo cat ./toolchains/arm/sources.list | sudo tee -a /etc/apt/sources.list
 sudo apt-get --yes update
-sudo apt-get --yes install libglfw3-dev:arm64 libopengl-dev:arm64 libportaudio2:arm64 portaudio19-dev:arm64 librtlsdr-dev:arm64 libfftw3-dev:arm64 libfftw3-single3:arm64
+
+# We need to install these manually for libglfw3 since apt doesn't seem to want to automatically install these
+sudo apt-get --yes install libllvm15:arm64
+sudo apt-get --yes install libgl1-mesa-dri:arm64
+sudo apt-get --yes install libglfw3-dev:arm64 libopengl-dev:arm64 
+sudo apt-get --yes libportaudio2:arm64 portaudio19-dev:arm64 librtlsdr-dev:arm64 libfftw3-dev:arm64 libfftw3-single3:arm64
